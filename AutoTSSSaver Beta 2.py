959333430e375ebd182a5e7d3bef0c3f8efc50a9
Lunarixus <35746622+Lunarixus@users.noreply.github.com>
2018-04-12 17:01:22 +0100

    Fixed automatic version updates, no more blob spam

---
+++ AutoTSSSaver Beta 2.py
@@ -0,0 +1,36 @@
+#Import modules for the program
+import urllib.request
+import subprocess
+
+#Set the version for comparison (Change if you want but it auto updates)
+set_version = ('11.2.6')
+
+#Ask user for their device information
+device = input("What is your device identifier? (eg. iPhone10,6) ")
+ecid = input("What is your ECID? ")
+
+#Start a while loop
+while True:
+    #Contact iNeal's API and split to get latest signed version
+    url = 'http://api.ineal.me/tss/%s/includebeta' % (device)
+    request_text = urllib.request.urlopen(url)
+    returned_values = request_text.read()
+    current_firmware = returned_values.decode('utf-8')
+    current_signed_version = current_firmware.split('"')[31]
+
+    #Start another loop for saving the blobs
+    while True:
+        #Save blobs if the current signed version is higher than latest
+        if current_signed_version > set_version:
+            print("A new firmware is signed!")
+            
+            subprocess.call(['tsschecker_windows.exe', '-d', device, '-l', '-e', ecid, '-s'])
+            
+            set_version = current_signed_version
+
+            break
+        else:
+            #If the latest version is 11.2.5 then print this repeatedly
+            print("Latest is", current_signed_version)
+            #This is here to stop us accidentally DoSing iNeal
+            time.sleep(4)
